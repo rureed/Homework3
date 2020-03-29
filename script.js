dc7e051d5a67dd200f6f8c6ee5d313c032f3cd36
Russ Reed <Russ@Macintosh.attlocal.net>
2020-03-28 21:23:34 -0400

    updating files

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var setLength;
-var useNum;
-var useLo;
-var useUp;
-var useSym;
 
-var ltrs = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
-var nums = ["1234567890"]
-var syms = ["!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+
+function generatePassword () {
+  var setLength = prompt("Choose password length: 8 - 128 characters.");
+  var useNum = confirm("Include numerals?");
+  var useLo = confirm("Include lower case?");
+  var useUp = confirm("Include upper case?");
+  var useSym = confirm("Include special characters?");
+  var nums = "1234567890";
+  var ups = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lows = "abcdefghijklmnopqrstuvwxyz";
+  var syms = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var choice = "";
+  var length = "";
+
+  // var password = ""
+  
+  if(setLength === ""){
+    alert("Please set a valid length from 8 - 128.");
+  } else if (setLength < 8 || setLength > 128){
+      alert("Please set a valid length from 8 - 128.");
+  }
+  
+  if(useNum === true){
+    choice = choice + nums
+  } 
+  if(useLo === true){
+    choice = choice + lows
+  }
+  if(useUp === true){
+    choice = choice + ups
+  }
+  if(useSym === true){
+    choice = choice + syms
+  }
+
+  return password
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  setLength = (Number(prompt("Choose password length from 8 - 128 characters.")));
-  if setLength = "" { 
-    alert("Please choose between 8 and 128 characters")
-
-  } else {
-    useNum = confirm("Will your password contain numbers?");
-    useLo = confirm("Will your password contain lower case letters?");
-    useUp = confirm("Will your password contain upper case letters?");
-    useSym = confirm("Will your password contain special characters?");
-  }
+ 
 
   passwordText.value = password;
 
