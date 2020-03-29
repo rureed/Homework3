e14b1da97c56f6bb529e37ba5f7aa878ccb527c9
Russ Reed <Russ@Macintosh.attlocal.net>
2020-03-29 00:32:27 -0400

    updating files

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
 function generatePassword () {
   var setLength = prompt("Choose password length: 8 - 128 characters.");
   var useNum;
@@ -11,12 +10,9 @@ function generatePassword () {
   var nums = "1234567890";
   var ups = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var lows = "abcdefghijklmnopqrstuvwxyz";
-  var syms = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  var syms = "!#$%&'()*+,-./:;<=>?@[^_`{|}~";
   var choice = "";
-  // var length = "";
-
   
-
   if(setLength === ""){
     alert("Please start over and set a VALID length from 8 - 128 characters.\n(SMH)");
       location.reload(forceget);
@@ -26,38 +22,40 @@ function generatePassword () {
   }
   
   useNum = confirm("Include numerals?");
-  if(useNum == true){
+  if(useNum === true){
     choice = choice.concat(nums);
   } 
   useLo = confirm("Include lower case?");
-  if(useLo == true){
+  if(useLo === true){
     choice = choice.concat(lows);
   }
   useUp = confirm("Include upper case?");
-  if(useUp == true){
+  if(useUp === true){
     choice = choice.concat(ups);
   }
   useSym = confirm("Include special characters?");
-  if(useSym == true){
+  if(useSym === true){
     choice = choice.concat(syms);
   }
-
   
   for (var i = 0; i < setLength; i++) {
     password += choice[Math.floor(Math.random() * choice.length)];
   }
-  console.log(setLength)
+  console.log(setLength, choice);
+
   return password
+  
 }
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
- 
+// I was unable to get rid of the [object HTMLTextAreaElement] barnacle attached to the beginning of the generated password. I tried adding .innerHTML as well as changing the querySelector to getElementById (and using .innerHTML again) to no avail. I just can't seem to find the wayward gremlin.
 
   passwordText.value = password;
 
+console.log(passwordText.value);
 }
 
 
